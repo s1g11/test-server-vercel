b04542efab5c5c1c0bdc96f1043f9636326b238a
Roman Petrakovskiy <105650706+s1g11@users.noreply.github.com>
2024-05-16 00:35:06 +0700

    add cors))

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import {productsRouter} from "./routes/products-router";
 import {getTestsRoutes} from "./routes/test";
 import {db} from "./db/db";
+import cors from "cors";
 
 export const app = express()
 export const jsonBodyMiddleware = express.json()
 
 app.use(jsonBodyMiddleware)
+app.use(cors())
 
 app.use('/products', productsRouter)
 
